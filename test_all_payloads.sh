56a02e2eb2083e120954dcb7134b40924c1e2eda
Wesley de Groot <email@wesleydegroot.nl>
2023-10-16 21:04:11 +0200

    Reorder tests

--- test_all_payloads.sh
+++ test_all_payloads.sh
@@ -1,4 +1,20 @@
-for filename in payload/*.json; do
-    php index.php ${filename#"payload/"}
-    sleep 5
-done
+# for filename in payload/*.json; do
+#     php index.php ${filename#"payload/"}
+#     sleep 5
+# done
+
+# Run test (issue.json)
+php index.php issue.json
+sleep 2
+
+# Run test (please_assign_me.json)
+php index.php please_assign_me.json
+sleep 2
+          
+# Run test (pr.json)
+php index.php pr.json
+sleep 2
+          
+# Run test (accept_pr.json)
+php index.php accept_pr.json
+sleep 2
