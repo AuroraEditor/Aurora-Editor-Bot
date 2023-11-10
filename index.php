cfdb5d6c2d781ac3feb36049434e500230ea340c
Wesley de Groot <email@wesleydegroot.nl>
2023-11-10 14:58:03 +0100

    Fix headers already sent

--- index.php
+++ index.php
@@ -63,7 +63,9 @@ if (PHP_SAPI === 'cli') {
 
 if (isset($_POST['payload'])) {
     // Comply with GitHub's webhook secret
-    header("HTTP/1.1 200 OK");
+    if (!headers_sent()) {
+        header("HTTP/1.1 200 OK");
+    }
 
     // Save payload log for traceability
     file_put_contents(
